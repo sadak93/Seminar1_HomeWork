a5cfdcb303426af3d1a2cf1e47fdd32f7812be9f
Sadak <s.sadak02@gmail.com>
2023-05-23 16:13:18 +0600

    Task 02 корректировка

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -11,8 +11,8 @@ System.Console.Write("Введите 2ое число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 if (num1>num2)
-    System.Console.WriteLine($"max = {num1}");
+    System.Console.WriteLine($"max = {num1}, min = {num2}");
 else if (num2>num1)
-    System.Console.WriteLine($"max = {num2}");
+    System.Console.WriteLine($"max = {num2}, min = {num1}");
 else 
     System.Console.WriteLine("Они равны");
